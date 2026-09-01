1d593a0de1ffca0b0576883b758e82c012b4aab3
IbrahemIsbieh <abrahemsbaih3@gmail.com>
2026-09-01 16:39:29 +0300

    APD on course_view & enrollment_view & student_viwe Add search

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'School Management System',
-    'version': '19.0',
+    'version': '19.0.1.0.0',
     'summary': 'School Management System',
     'author': 'ibrahim',
     'category': 'Services',
@@ -8,8 +8,8 @@
     'data': [  'security/ir.model.access.csv',
                'views/base_menu.xml',
                'views/student_view.xml',
-               'views/enrollment_view.xml',
                'views/course_view.xml',
+               'views/enrollment_view.xml',
 
     ],
     'installable': True,
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,3 @@
 from . import school_student
+from . import school_course
+from . import school_enrollment
